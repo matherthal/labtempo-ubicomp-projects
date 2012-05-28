e709a819db8987aebcf9530c9fdd9a54b9d34f7a
matherthal <matherthal@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-05-28 14:41:27 +0000

    first implementation of person, that actually should be implemented as an aggregator.... see how to do this

---
+++  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/resources/Person.java
@@ -0,0 +1,34 @@
+package br.uff.tempo.middleware.resources;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.json.JSONException;
+
+import br.uff.tempo.middleware.comm.Tuple;
+import br.uff.tempo.middleware.management.ResourceAgent;
+import br.uff.tempo.middleware.resources.interfaces.IPerson;
+
+public class Person extends ResourceAgent implements IPerson{
+
+	@Override
+	public void notificationHandler(String change) throws JSONException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Date getAge() {
+		return null;
+	}
+
+
+	@Override
+	public List<Tuple<String, Method>> getAttribs() throws SecurityException, NoSuchMethodException {
+		List<Tuple<String, Method>> attribs = new ArrayList<Tuple<String,Method>>();
+		attribs.add(new Tuple<String, Method>("Idade", this.getClass().getMethod("getAge")));
+		return attribs;
+	}
+}
