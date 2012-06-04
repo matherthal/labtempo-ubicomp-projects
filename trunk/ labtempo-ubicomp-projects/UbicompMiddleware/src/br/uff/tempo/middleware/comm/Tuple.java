bf246d52fc5d4207e96cc2ad01678d8b8b808adb
matherthal <matherthal@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-06-04 18:53:31 +0000

    added tuple <object, object>

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/comm/Tuple.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/comm/Tuple.java
@@ -1,11 +1,17 @@
 package br.uff.tempo.middleware.comm;
 
 public class Tuple<key, value> {
-	public final String key;
-	public final Object value;
-
+	public String key = "";
+	public Object value = null;
+	public Object value2 = null;
+	
 	public Tuple(String key, Object value) {
 		this.key = key;
 		this.value = value;
+	}
+	
+	public Tuple(Object value, Object value2) {
+		this.value = value;
+		this.value2 = value2;
 	}
 }
