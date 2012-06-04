54054238cbfb26e75d49cb47bab88ee8243d2d98
matherthal <matherthal@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-06-04 19:01:32 +0000

    implementing aggregator and interpreter

---
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/Aggregator.java
@@ -0,0 +1,27 @@
+package br.uff.tempo.middleware.management;
+
+import org.json.JSONException;
+
+import br.uff.tempo.middleware.management.interfaces.IAggregator;
+import br.uff.tempo.middleware.management.interfaces.IResourceDiscovery;
+import br.uff.tempo.middleware.resources.interfaces.IStove;
+import br.uff.tempo.middleware.resources.stubs.StoveStub;
+
+public class Aggregator extends ResourceAgent implements IAggregator {
+	@Override
+	public boolean addContextVariable(Interpreter interpreter) {
+		this.contextVariables.add(interpreter);
+		
+		IResourceDiscovery iRDS = getRDS();
+		IStove iStove = new StoveStub(iRDS.search("stove").get(0));
+		iStove.registerStakeholder("getOvenIsOn", this.getURL());
+		
+		return false;
+	}
+
+	@Override
+	public void notificationHandler(String change) {
+		// TODO Auto-generated method stub
+		
+	}
+}
