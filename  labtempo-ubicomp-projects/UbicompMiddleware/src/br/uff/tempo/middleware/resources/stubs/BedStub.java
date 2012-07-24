13fe223b02b0e27cb60398cb9d07329b898ae6db
douglasropb@gmail.com <douglasropb@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-07-24 00:36:30 +0000

    context variables being updated

---  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/resources/stubs/BedStub.java
+++  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/resources/stubs/BedStub.java
@@ -3,7 +3,7 @@ package br.uff.tempo.middleware.resources.stubs;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.uff.tempo.middleware.comm.Tuple;
+import br.uff.tempo.middleware.comm.current.api.Tuple;
 import br.uff.tempo.middleware.management.stubs.ResourceAgentStub;
 import br.uff.tempo.middleware.resources.interfaces.IBed;
 
