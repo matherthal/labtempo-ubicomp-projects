6085f17dfabf70ae588ecc8d893b8f59de1a6694
andre.ribeiro.coelho@gmail.com <andre.ribeiro.coelho@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-09-08 17:35:48 +0000

    Refactory on interest api. Updating REPA .jar and .so.

---
+++  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/comm/interest/api/CommREPAD.java
@@ -0,0 +1,76 @@
+package br.uff.tempo.middleware.comm.interest.api;
+
+import java.net.SocketException;
+
+import ufrj.coppe.lcp.repa.PrefixAddress;
+import ufrj.coppe.lcp.repa.RepaMessage;
+import ufrj.coppe.lcp.repa.RepaSocket;
+
+public class CommREPAD {
+
+	private Thread myThread;
+
+	private RepaSocket repaSocket;
+
+	public CommREPAD() throws SocketException {
+		repaSocket = RepaSocket.getRepaSocket();
+		repaSocket.repaOpen();
+
+		myThread = new Thread(new Runnable() {
+			public void run() {
+				try {
+					while (true) {
+						new REPASession(repaSocket.repaRecv());
+					}
+				} catch (Exception e) {
+				}
+			}
+		});
+		myThread.setDaemon(true);
+		myThread.start();
+	}
+	
+	public String serve(RepaMessage repaMessage) {
+		return null;
+	}
+
+	private class REPASession implements Runnable {
+
+		private RepaMessage repaMessage;
+
+		public REPASession(RepaMessage repaMessage) {
+			this.repaMessage = repaMessage;
+			
+			Thread t = new Thread(this);
+			t.setDaemon(true);
+			t.start();
+		}
+
+		public void run() {
+			if (this.repaMessage == null) {
+				return;
+			}
+
+			String r = serve(this.repaMessage);
+			if (r != null) {
+				//sendResponse(r.status, r.mimeType, r.header, r.data);
+			}
+		}
+	}
+
+	public PrefixAddress getRepaNodeAdress() {
+		return this.repaSocket.getRepaNodeAdress();
+	}
+
+	public void repaSend(RepaMessage repaMessage) throws Exception {
+		this.repaSocket.repaSend(repaMessage);
+	}
+
+	public void registerInterest(String interest) throws Exception {
+		this.repaSocket.registerInterest(interest);
+	}
+
+	public void unregisterInterest(String interest) throws Exception {
+		this.repaSocket.unregisterInterest(interest);
+	}
+}
