db5fa243c123e6cba47fd088cfc7b52547cce3b9
david.brto@gmail.com <david.brto@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-09-11 20:21:08 +0000

    Creating a Log interface...

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/interfaces/IResourceDiscovery.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/interfaces/IResourceDiscovery.java
@@ -8,7 +8,7 @@ import br.uff.tempo.middleware.management.utils.ResourceAgentIdentifier;
 public interface IResourceDiscovery {
 	//public static final String RDS_ADDRESS = "rai:" + ResourceAgentIdentifier.getLocalIpAddress() + "//br.uff.tempo.middleware.management.ResourceDiscovery:ResourceDiscovery";
 	//public static final String RDS_ADDRESS = "rai:192.168.1.102//br.uff.tempo.middleware.management.ResourceDiscovery:ResourceDiscovery";
-	public static final String RDS_ADDRESS = "rai:192.168.0.100//br.uff.tempo.middleware.management.ResourceDiscovery:ResourceDiscovery";
+	public static final String RDS_ADDRESS = "rai:192.168.1.115//br.uff.tempo.middleware.management.ResourceDiscovery:ResourceDiscovery";
 
 	public ArrayList<String> search(String query);// return URI of resources
 
