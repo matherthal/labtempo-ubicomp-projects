cb6ff3e7d9f2fab8e5a2b45589ceb28b8bbdddb3
david.brto@gmail.com <david.brto@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-09-12 15:32:16 +0000

    Added a method in MapActivity to load a "logical map" in the ResourceLocation Fixed the Stub and Interface from ResourceLocation Changed coodinates from Position to float

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/ResourceLocation.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/ResourceLocation.java
@@ -7,14 +7,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import android.util.Log;
 import br.uff.tempo.middleware.comm.current.api.Tuple;
 import br.uff.tempo.middleware.management.interfaces.IResourceLocation;
 import br.uff.tempo.middleware.management.utils.Position;
 import br.uff.tempo.middleware.management.utils.ResourceAgentIdentifier;
 import br.uff.tempo.middleware.management.utils.Sorter;
 
-public class ResourceLocation extends ResourceAgent implements IResourceLocation {
-	
+public class ResourceLocation extends ResourceAgent implements
+		IResourceLocation {
+
 	private static final long serialVersionUID = 1L;
 
 	private ResourceRepository rR;
@@ -24,47 +26,49 @@ public class ResourceLocation extends ResourceAgent implements IResourceLocation
 	HashMap<String, Place> map;
 	HashMap<String, Position> resources;
 	HashMap<String, HashMap<String, Position>> base;
-		
+
 	private ResourceLocation() {
 		setId(3);
 		setRAI("br.uff.tempo.middleware.management.ResourceLocation");
 		setName("ResourceLocation");
 		setType("management");
 
-		setRAI(ResourceAgentIdentifier.generateRAI(ResourceAgentIdentifier.getLocalIpAddress(), "br.uff.tempo.middleware.management.ResourceLocation", "ResourceLocation"));
+		setRAI(ResourceAgentIdentifier.generateRAI(
+				ResourceAgentIdentifier.getLocalIpAddress(),
+				"br.uff.tempo.middleware.management.ResourceLocation",
+				"ResourceLocation"));
 
 		map = new HashMap<String, Place>();
 		resources = new HashMap<String, Position>();
 		base = new HashMap<String, HashMap<String, Position>>();
 		ResourceContainer.getInstance().add(this);
-		loadBase();
+		// loadBase();
 	}
-	
-	
+
 	//
 	//
 	/**
 	 * carrega mapa com lugares baseados na interface do MapaDaCasa areas da
 	 * casa e dos comodos foram estimadas area da casa: 100x50
 	 */
-	public void loadBase()
-	{
-		addPlace("Hall", new Position(0,0), new Position(50,25));		
-		addPlace("Bathroom", new Position(50,0), new Position(70,20));
-		addPlace("Storeroom", new Position(70,0), new Position(100,20));
-		addPlace("Corridor", new Position(50,20), new Position(100,25));
-		addPlace("Bedroom1", new Position(0,25), new Position(30,50));
-		addPlace("Bedroom2", new Position(30,25), new Position(60,50));
-		addPlace("Kitchen", new Position(60,25), new Position(100,50));
+	public void loadBase() {
+		addPlace("Hall", new Position(0, 0), new Position(50, 25));
+		addPlace("Bathroom", new Position(50, 0), new Position(70, 20));
+		addPlace("Storeroom", new Position(70, 0), new Position(100, 20));
+		addPlace("Corridor", new Position(50, 20), new Position(100, 25));
+		addPlace("Bedroom1", new Position(0, 25), new Position(30, 50));
+		addPlace("Bedroom2", new Position(30, 25), new Position(60, 50));
+		addPlace("Kitchen", new Position(60, 25), new Position(100, 50));
 	}
-	
+
 	public void addPlace(String name, Position lower, Position upper) {
 		Place place = new Place(name, lower, upper);
 		//place.identify();
 		map.put(place.getName(), place);
 		base.put(place.getName(), new HashMap<String, Position>());
+		
+		Log.d("SmartAndroid", "New place added: " + name + ". Bottom-left corner at = [" + lower.getX() + " , " + lower.getY() + "] and top-right corner at [" + upper.getX() + " , " + upper.getY() + "]");
 	}
-	
 
 	public static ResourceLocation getInstance() {
 		if (instance == null)
@@ -72,29 +76,29 @@ public class ResourceLocation extends ResourceAgent implements IResourceLocation
 		return instance;
 	}
 
-//	public ArrayList<Place> search(ResourceAgent rA) {
-//		ArrayList<Place> result = new ArrayList<Place>();
-//		Place current = get(rA);
-//		result.add(0, current);// current local is the nearest
-//		ArrayList<Place> localList = (ArrayList<Place>) map.values();
-//		result = getNeighbors(current, localList, result);
-//		return result;
-//	}
-	
+	// public ArrayList<Place> search(ResourceAgent rA) {
+	// ArrayList<Place> result = new ArrayList<Place>();
+	// Place current = get(rA);
+	// result.add(0, current);// current local is the nearest
+	// ArrayList<Place> localList = (ArrayList<Place>) map.values();
+	// result = getNeighbors(current, localList, result);
+	// return result;
+	// }
+
 	public ArrayList<String> search(String query) {
 		ArrayList<String> result = new ArrayList<String>();
 		ArrayList<Set<String>> setList = new ArrayList<Set<String>>();
-		
+
 		if (query.equals("")) {
 			Set<String> setMap = base.keySet();
 			Iterator<String> itSet = setMap.iterator();
-			while(itSet.hasNext()) {
-				Map<String,Position> raMap = base.get(itSet.next());
+			while (itSet.hasNext()) {
+				Map<String, Position> raMap = base.get(itSet.next());
 				setList.add(raMap.keySet());
 			}
 		} else {
-			Map<String,Position> raMap = base.get(query);
-			setList.add(raMap.keySet());		
+			Map<String, Position> raMap = base.get(query);
+			setList.add(raMap.keySet());
 		}
 		for (Set<String> set : setList) {
 			Iterator<String> itRA = set.iterator();
@@ -102,15 +106,17 @@ public class ResourceLocation extends ResourceAgent implements IResourceLocation
 				result.add(itRA.next());
 			}
 		}
-			
+
 		return result;
 	}
 
-	private ArrayList<Place> getNeighbors(Place current, ArrayList<Place> localList, ArrayList<Place> result) {
+	private ArrayList<Place> getNeighbors(Place current,
+			ArrayList<Place> localList, ArrayList<Place> result) {
 		ArrayList<Place> neighborList = new ArrayList<Place>();
 		if (localList.size() != 0) {
 			for (Place local : localList) {
-				if (current.equalCorners(local) || current.equalSide(local) || current.equalHeight(local)) {
+				if (current.equalCorners(local) || current.equalSide(local)
+						|| current.equalHeight(local)) {
 					result.add(local);
 					neighborList.add(local);
 				}
@@ -122,7 +128,6 @@ public class ResourceLocation extends ResourceAgent implements IResourceLocation
 		}
 		return result;
 	}
-	
 
 	public Place get(ResourceAgent rA) {
 		// TODO Auto-generated method stub
@@ -130,16 +135,15 @@ public class ResourceLocation extends ResourceAgent implements IResourceLocation
 	}
 
 	public Place getLocal(Position position) {
-		
-		for (Place local : map.values())
-		{
-			if (local.contains(position)){
+
+		for (Place local : map.values()) {
+			if (local.contains(position)) {
 				return local;
 			}
 		}
 		return null;
 	}
-	
+
 	public void setMap(HashMap<String, Place> map) {
 		this.map = map;
 	}
@@ -155,53 +159,50 @@ public class ResourceLocation extends ResourceAgent implements IResourceLocation
 		rAMap.put(url, position);
 		resources.put(url, position);
 	}
-	
-	public void registerInPlaceRelative(String url, Place place, Position position) {
+
+	public void registerInPlaceRelative(String url, Place place,
+			Position position) {
 		HashMap<String, Position> rAMap = base.get(place.getName());
-		int x = place.lower.getX() + position.getX();
-		int y = place.lower.getY() + position.getY();
-		Position rPos = new Position(x,y);
+		float x = place.lower.getX() + position.getX();
+		float y = place.lower.getY() + position.getY();
+		Position rPos = new Position(x, y);
 		rAMap.put(url, rPos);
 		resources.put(url, rPos);
 	}
-	
+
 	public void registerInPlaceMiddlePos(String url, Place place) {
-		int x = Integer.valueOf((place.lower.getX()+place.upper.getX()/2));
-		int y = Integer.valueOf((place.lower.getY()+place.upper.getY()/2));
-		Position position = new Position(x,y); 
+		float x = Float.valueOf((place.lower.getX() + place.upper.getX() / 2));
+		float y = Float.valueOf((place.lower.getY() + place.upper.getY() / 2));
+		Position position = new Position(x, y);
 		HashMap<String, Position> rAMap = base.get(place.getName());
 		rAMap.put(url, position);
 		resources.put(url, position);
 	}
-	
-	public Set<String> listLocations()
-	{
+
+	public Set<String> listLocations() {
 		return map.keySet();
 	}
-
 
 	@Override
 	public Position getPosition(String place, String rai) {
 		return base.get(place).get(rai);
 	}
-	
+
 	public ArrayList<String> queryByLocal(Position position) {
-		List<Tuple<String,Position>> raList = new ArrayList<Tuple<String,Position>>();
-		Set<String> setRai= resources.keySet();
+		List<Tuple<String, Position>> raList = new ArrayList<Tuple<String, Position>>();
+		Set<String> setRai = resources.keySet();
 		Iterator<String> itRai = setRai.iterator();
-		while(itRai.hasNext()) {
+		while (itRai.hasNext()) {
 			String rai = itRai.next();
 			raList.add(new Tuple(rai, resources.get(rai)));
 		}
-		
-		Sorter<Position> sorter = new Sorter<Position>(raList,position);
+
+		Sorter<Position> sorter = new Sorter<Position>(raList, position);
 		return sorter.sort();
 	}
-	
-	public Place getPlace(String name)
-	{
+
+	public Place getPlace(String name) {
 		return map.get(name);
 	}
-	
-	
+
 }
