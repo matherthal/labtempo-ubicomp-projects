69da76eba99171e7975022990a533085287a1278
david.brto@gmail.com <david.brto@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-09-18 21:54:28 +0000

    Added some context elements, and using notifyStakeholders(). Minor changes

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/resources/Stove.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/resources/Stove.java
@@ -14,18 +14,29 @@ public class Stove extends ResourceAgent implements IStove {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private static final String TAG = "Stove";
+	//Constants
+	public static final int BURNERS_NUMBER = 4;
+	
+	//Context Elements
+	public static final String CV_ISON = "isOn";
+	public static final String CV_BURNER_ON_1 = "isBurnerOn1";
+	public static final String CV_BURNER_ON_2 = "isBurnerOn2";
+	public static final String CV_BURNER_ON_3 = "isBurnerOn3";
+	public static final String CV_BURNER_ON_4 = "isBurnerOn4";
+	private static final String BURNER_ON = "isBurnerOn"; //used in a concat operation
+	public static final String CV_OVEN_ON = "isOvenOn";
+	public static final String CV_OVEN_TEMPERATURE = "ovenTemperature";
 
 	// Gas Leak
 	private float gasLeak = 0.0f; // Measure of natural gas leaking
+	
+	//Oven
 	private float ovenTemp = 0.0f;
 	private boolean ovenOn = false;
+	
+	private boolean stoveOn = false;
 
-	// private List<Object> components;
-	// private static final int SOUND_ID_STOVE = R.raw.stove;
-	// private static final int SOUND_ID_OVEN_OPEN = R.raw.ovenopen;
-	// private static final int SOUND_ID_OVEN_CLOSE = R.raw.ovenclose;
-
+	//Stove Burners
 	private List<Float> burners;
 
 	public Stove() {
@@ -34,49 +45,20 @@ public class Stove extends ResourceAgent implements IStove {
 
 	public Stove(String name) {
 		// FIXME: get correct id
-		super(name, "br.uff.tempo.middleware.resources.Stove", 5);
-		burners = new ArrayList<Float>(4);
-		initBurners();
+		this(name, new Position(0, 0));
 	}
 	
 	public Stove(String name, Position position) {
 		// FIXME: get correct id
 		super(name, "br.uff.tempo.middleware.resources.Stove", 5, position);
-		burners = new ArrayList<Float>(4);
 		initBurners();
 	}
 
 	private void initBurners() {
-		for (int i = 0; i < 4; i++)
+		
+		burners = new ArrayList<Float>(BURNERS_NUMBER);
+		for (int i = 0; i < BURNERS_NUMBER; i++)
 			burners.add(0.0f);
-	}
-
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		if (burners.size() < 4) {
-			burners = new ArrayList<Float>(4);
-		}
-	}
-
-	/*
-	 * Class for clients to access. Because we know this service always runs in
-	 * the same process as its clients, we don't need to deal with IPC.
-	 */
-	public class StoveBinder extends Binder {
-		Stove getService() {
-			return Stove.this;
-		}
-	}
-
-	public StoveBinder mBinder = new StoveBinder();
-
-	public IBinder onBind() {
-		return mBinder;
-	}
-
-	@Override
-	public void notificationHandler(String rai, String method, Object value) {
 	}
 
 	/*
@@ -100,23 +82,36 @@ public class Stove extends ResourceAgent implements IStove {
 
 	@Override
 	public boolean isOn() {
-		// TODO Auto-generated method stub
-		return false;
+		
+		boolean ret = true;
+		for (float value : burners) {
+			if (value == 0f) {
+				ret = false;
+			}
+		}
+		
+		//all elements from the stove must be on (burners and oven)
+		ret = ret && ovenOn;
+		
+		notifyStakeholders(CV_ISON, ret);
+		return ret;
 	}
 
 	@Override
 	public void turnOnBurner(int burnerIndex) {
 		burners.set(burnerIndex, 1.0f);
+		notifyStakeholders(BURNER_ON + burnerIndex, true);
 	}
 
 	@Override
 	public void turnOffBurner(int burnerIndex) {
 		burners.set(burnerIndex, 0.0f);
+		notifyStakeholders(BURNER_ON + burnerIndex, false);
 	}
 
 	@Override
 	public boolean isOnBurner(int burnerIndex) {
-		return burners.get(burnerIndex) > 0;
+		return burners.get(burnerIndex) > 0f;
 	}
 
 	@Override
@@ -124,23 +119,21 @@ public class Stove extends ResourceAgent implements IStove {
 		this.ovenTemp = newTemperature;
 		this.ovenOn = true;
 		
-		notifyStakeholders("getOvenTemperature", newTemperature);
+		notifyStakeholders(CV_OVEN_TEMPERATURE, newTemperature);
 	}
 
 	@Override
 	public float getOvenTemperature() {
-		// Random r = new Random();
-		// this.tempOven1 = 80 + r.nextFloat() * 20;
-		// return this.tempOven1;
-		// TODO Auto-generated method stub
 
 		return this.ovenTemp;
 	}
 
 	@Override
 	public void turnOffOven() {
-		// TODO Auto-generated method stub
-
+		
+		this.ovenTemp = 0f;
+		
+		notifyStakeholders(CV_OVEN_ON, false);
 	}
 
 	@Override
@@ -152,5 +145,9 @@ public class Stove extends ResourceAgent implements IStove {
 	@Override
 	public boolean isOvenOn() {
 		return this.ovenOn;
+	}
+	
+	@Override
+	public void notificationHandler(String rai, String method, Object value) {
 	}
 }
