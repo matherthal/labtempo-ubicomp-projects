c8209d688fc60abc054b47718b1f5ca0d1446195
david.brto@gmail.com <david.brto@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-09-19 20:22:32 +0000

    Added an agent in every Panel, listening events from the simulator one. This way, it's possible update the View screen when a external agent change the View agent state.

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/simulators/AbstractPanel.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/simulators/AbstractPanel.java
@@ -1,15 +1,20 @@
 package br.uff.tempo.apps.simulators;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.WindowManager;
+import br.uff.tempo.middleware.management.interfaces.IResourceAgent;
+import br.uff.tempo.middleware.resources.Generic;
 
 public abstract class AbstractPanel extends View {	
 
 	private int screenCenterX;
 	private int screenCenterY;
+	
+	private Handler handler = new Handler();
 
 	public AbstractPanel(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -24,6 +29,30 @@ public abstract class AbstractPanel extends View {
 		//get the screen center coordinates
 		screenCenterX = metrics.widthPixels / 2;
 		screenCenterY = metrics.heightPixels / 2;
+		
+		setupInterest();
+	}
+	
+	public void setupInterest() {
+		
+		// When the agent change its state, redraw the screen
+		IResourceAgent res = ((AbstractView) getContext()).getAgent();
+		
+		new Generic("Stakeholder", res, "all") {
+			
+			@Override
+			public void notificationHandler(String rai, String method, Object value) {
+				
+				handler.post(new Runnable() {
+					
+					@Override
+					public void run() {
+						AbstractPanel.this.invalidate();	
+					}
+				});
+				
+			}
+		};
 	}
 
 	public int getScreenCenterX() {
@@ -33,7 +62,4 @@ public abstract class AbstractPanel extends View {
 	public int getScreenCenterY() {
 		return screenCenterY;
 	}
-
-	
-
 }
