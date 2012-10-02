3d90699e55cfbc5e056756e599769546e40a2456
david.brto@gmail.com <david.brto@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-10-02 20:47:17 +0000

    Trying to implement a Person position notification periodically

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/Person.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/Person.java
@@ -1,80 +1,128 @@
 package br.uff.tempo.middleware.management;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import br.uff.tempo.apps.map.objects.ResourceObject;
 import br.uff.tempo.middleware.management.interfaces.IResourceLocation;
 import br.uff.tempo.middleware.management.stubs.ResourceLocationStub;
 import br.uff.tempo.middleware.management.utils.Position;
 
 public class Person extends ResourceAgent {
-	
+
+	public static final long DEFAULT_PERIOD = 1000;
+	public static final int DEFAUT_WINDOW_SIZE = 20;
 	private static final long serialVersionUID = 1L;
+	private static final String CV_POSITION = "position";
+
+	private Timer timer;
+
+	private long period; // milliseconds
+	private int windowSize;
+
+	Position lastPos;
 
 	List<SmartObject> objects;
 	List<Place> recentLocal;
-	List<Position> recentPositions;
+	LinkedList<Position> recentPositions;
 	IResourceLocation rLS;
-	public Person(List<SmartObject> sensors, List<Position> recentPositions, String name, String type, int id){
+
+	public Person(String name) {
+		this(name, Person.class.getName(), DEFAULT_PERIOD);
+	}
+
+	public Person(String name, String type, long period) {
+		this(new ArrayList<SmartObject>(), new LinkedList<Position>(), name,
+				type, 4312, period);
+
+	}
+
+	public Person(List<SmartObject> sensors,
+			LinkedList<Position> recentPositions, String name, String type,
+			int id, long period) {
 		super(name, type, id);
 		this.objects = sensors;
-		this.recentPositions = recentPositions;		
+		this.recentPositions = recentPositions;
+
+		this.period = period;
+
+		this.windowSize = DEFAUT_WINDOW_SIZE;
+
+		this.timer = new Timer();
+
+		timer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+
+				while (true) {
+					if (lastPos != null) {
+						notifyStakeholders(CV_POSITION, lastPos);
+						lastPos = null;
+					}
+				}
+			}
+		}, this.period);
 	}
-	
-	public void updateRecentLocal()
-	{
-		rLS = new ResourceLocationStub(this.getRDS().search("ResourceLocation").get(0));
-		for (Position position : recentPositions)
-		{
+
+	public void updateRecentLocal() {
+		rLS = new ResourceLocationStub(this.getRDS().search("ResourceLocation")
+				.get(0));
+		for (Position position : recentPositions) {
 			recentLocal.add(rLS.getLocal(position));
 		}
 	}
-	
-	//list is in reverse order
-	public SmartObject getSensor(int i)
-	{
-		return objects.get(objects.size()-(i+1));
+
+	// list is in reverse order
+	public SmartObject getSensor(int i) {
+		return objects.get(objects.size() - (i + 1));
 	}
-	
-	//return preview sensor
-	public SmartObject setSensor(int i, SmartObject sensor){
-		return objects.set(objects.size()-(i+1), sensor);
+
+	// return preview sensor
+	public SmartObject setSensor(int i, SmartObject sensor) {
+		return objects.set(objects.size() - (i + 1), sensor);
 	}
-	
-	public void addSensor(SmartObject sensor)
-	{
+
+	public void addSensor(SmartObject sensor) {
 		objects.add(sensor);
 	}
-	
-	public boolean removeSensor(SmartObject sensor)
-	{
+
+	public boolean removeSensor(SmartObject sensor) {
 		return objects.remove(sensor);
 	}
-	
-	public Position getPosition(int i)
-	{
-		return recentPositions.get(objects.size()-(i+1));
+
+	public Position getPosition(int i) {
+		return recentPositions.get(objects.size() - (i + 1));
 	}
-	
-	public Position getCurrentPosition()
-	{
-		return getPosition(0);
+
+	public Position getCurrentPosition() {
+		return recentPositions.getLast();
 	}
-	
-	//return preview sensor
-	public Position setPosition(int i, Position position){
-		return recentPositions.set(recentPositions.size()-(i+1), position);
-	}
-	
-	public void addPosition(Position position)
-	{
+
+	// //return preview sensor
+	// public Position setPosition(int i, Position position){
+	// return recentPositions.set(recentPositions.size()-(i+1), position);
+	// }
+
+	public void addPosition(Position position) {
+		if (recentPositions.size() >= this.windowSize) {
+			recentPositions.removeFirst();
+		}
+
 		recentPositions.add(position);
+
+		lastPos = position;
+
+		// notifyStakeholders(CV_POSITION, position);
 	}
-	
-	public boolean removePosition(Position position)
-	{
+
+	public boolean removePosition(Position position) {
 		return recentPositions.remove(position);
-	}	
-	
+	}
+
 	@Override
 	public void notificationHandler(String rai, String method, Object value) {
 		// TODO Auto-generated method stub
