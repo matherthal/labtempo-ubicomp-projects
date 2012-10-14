15fb41a41f0988297d84ee4972c0e0210baa1167
david.brto <david.brto@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-10-14 03:42:10 +0000

    Now we can remove an stakeholder from any Agent through the Map, by the Settings Menu

---  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/settings/MapSettings.java
+++  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/settings/MapSettings.java
@@ -1,12 +1,16 @@
 package br.uff.tempo.apps.map.settings;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.app.Dialog;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.widget.Toast;
 import br.uff.tempo.R;
 import br.uff.tempo.apps.map.dialogs.ChooseResource;
 import br.uff.tempo.apps.map.dialogs.IResourceChooser;
@@ -15,19 +19,27 @@ import br.uff.tempo.apps.map.dialogs.MiddlewareOperation;
 import br.uff.tempo.middleware.management.interfaces.IResourceDiscovery;
 import br.uff.tempo.middleware.management.interfaces.IResourceRegister;
 import br.uff.tempo.middleware.management.stubs.ResourceAgentStub;
-import br.uff.tempo.middleware.management.stubs.ResourceDiscoveryStub;
 import br.uff.tempo.middleware.management.stubs.ResourceRegisterStub;
+import br.uff.tempo.middleware.management.utils.Stakeholder;
 
 public class MapSettings extends PreferenceActivity implements IResourceListGetter, IResourceChooser {
+	
+	public static int OP_UNREG = 0;
+	public static int OP_SETUP = 1;
+	public static int OP_STAKEHOLDER = 2;
 	
 	private Preference resPref;
 	private Preference stakeholderPref;
 	private ChooseResource choose;
+	private ChooseResource sHolder;
 	private List<String> list;
 	private static String rdsAddress;
-	private boolean unregister = false;
+	private int op;
 	private String raiRegister;
 	private IResourceRegister reg = null;
+	private List<Stakeholder> stakeholders;
+	private ResourceAgentStub current; 
+	private Map<String, String> sHolderNames;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +50,9 @@ public class MapSettings extends PreferenceActivity implements IResourceListGett
 		new MiddlewareOperation(this, "", getRDSAddress()).execute(null);
 		
 		choose = new ChooseResource(this);
+		sHolder = new ChooseResource(this);
+		
+		sHolderNames = new HashMap<String, String>();
 		
 		resPref = findPreference("regResources");
 		resPref.setOnPreferenceClickListener(new OnPreferenceClickListener() {
@@ -60,33 +75,52 @@ public class MapSettings extends PreferenceActivity implements IResourceListGett
 
 	public boolean unregisterResourceClick() {
 		
-		unregister = true;
+		op = OP_UNREG;
 		choose.showDialog(list);
 		return false;
 	}
 	
 	public boolean setupStakeholdersClick() {
 		
-		unregister = false;
+		op = OP_SETUP;
+		choose.showDialog(list);
 		return false;
 	}
 	
 	@Override
-	public void onRegisteredResourceChoosed(String resourceRAI) {
+	public void onRegisteredResourceChoosed(String resource) {
 		
-		//ResourceAgentStub res = new ResourceAgentStub(resourceRAI);
-		
-		if (unregister) {
+		// Unregister an Agent
+		if (op == OP_UNREG) {
 			
 			if (reg == null) {
 				reg = new ResourceRegisterStub(raiRegister);
 			}
 			
-			reg.unregister(resourceRAI);
+			reg.unregister(resource);
 			new MiddlewareOperation(this, "", getRDSAddress()).execute(null);
 			
-		} else {
+		// Setup the stakeholders from an Agent
+		} else if (op == OP_SETUP){
+		
+			current = new ResourceAgentStub(resource);
+			stakeholders = current.getStakeholders();
+			List<String> shStrings = new ArrayList<String>();
 			
+			for (Stakeholder s : stakeholders) {
+				shStrings.add(s.toString());
+				sHolderNames.put(s.getName(), s.getRAI());
+			}
+			
+			op = OP_STAKEHOLDER;
+			choose.dismiss();
+			sHolder.showDialog(shStrings, false);
+		
+		// A stakeholder was selected
+		} else if (op == OP_STAKEHOLDER) {
+			
+			String[] parsed = resource.split(" wants: ");
+			current.removeStakeholder(parsed[1], sHolderNames.get(parsed[0]));
 		}
 	}
 
@@ -101,6 +135,7 @@ public class MapSettings extends PreferenceActivity implements IResourceListGett
 		
 		raiRegister = null;
 		
+		// Save the ResourceRegister RAI to avoid calling a remote method
 		for (String rai : list) {
 			if (rai.contains("ResourceRegister")) {
 				raiRegister = rai;
