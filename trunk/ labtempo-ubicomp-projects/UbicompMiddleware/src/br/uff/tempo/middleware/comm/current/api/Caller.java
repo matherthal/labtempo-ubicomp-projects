137e4c050b26fbad2c75a3ff7ffbc6becae3306b
andre.ribeiro.coelho@gmail.com <andre.ribeiro.coelho@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-10-15 19:15:38 +0000

    Removing unused variables and method.

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/comm/current/api/Caller.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/comm/current/api/Caller.java
@@ -13,24 +13,11 @@ public class Caller implements Serializable {
 	
 	private ResourceAgentIdentifier calleeAgent;
 
-	private int port = 8080;
-	SocketService socket;
-
-	// private Handler handler = new Handler();
-
 	public Caller(String calleeAgent) {
 		this.calleeAgent = new ResourceAgentIdentifier(calleeAgent);
-		// socket = new SocketService(this.calleeAgent.getPath(), 8080);
-		// serverIP = "192.168.1.70"; //FIXME: IP shouldn't be hardcoded
-	}
-
-	public String getAgentCaller() {
-		return calleeAgent.getType() + ":" + calleeAgent.getName();
 	}
 
 	public String sendMessage(String jsonString) {
-		// add callee + methodCaller to JSONObject
-
 		try {
 			String local = ResourceAgentIdentifier.getLocalIpAddress();
 			String result = "";
