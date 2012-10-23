dbd846c52f1093884f6731953a860602215f2267
david.brto <david.brto@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-10-23 01:30:52 +0000

    Now, the map can save and load its state when the app is destroyed and resumed. Besides, it can keep the state of a simulator GUI, e.g: open a lamp simulator through the map, turn it on, back to the map, open that GUI again and it will continue turned on! (it's really amazing this function didn't exist yet!)
    P.S: The problem: I was serializing an agent, so a change in this agent didn't affect the other one (because they were different objects, due the serialization). Now I serialize a stub instead, and the changes are notified to the original agent.

---  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/objects/INotificationBoxReceiver.java
+++  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/objects/INotificationBoxReceiver.java
@@ -1,5 +1,7 @@
 package br.uff.tempo.apps.map.objects;
 
+import java.io.Serializable;
+
 import org.andengine.util.color.Color;
 
 public interface INotificationBoxReceiver {
---  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/objects/RegistryData.java
+++  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/objects/RegistryData.java
@@ -2,6 +2,8 @@ package br.uff.tempo.apps.map.objects;
 
 import java.io.Serializable;
 
+import br.uff.tempo.middleware.management.interfaces.IResourceAgent;
+
 /**
  * This class aims to wrap the necessary information to register a resource and
  * enable this information to be passed as parameter easily. Initially, it's
@@ -13,37 +15,37 @@ public class RegistryData implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
-	private String resName;
-	private float posX;
-	private float posY;
+	private String resourceName;
+	private float positionX;
+	private float positionY;
+	private IResourceAgent agent;
+	private IResourceAgent stub;
+
+	// fields used as map information
+	private boolean fake;
+	private int iconType;
 	
 	public RegistryData() {	}
 	
 	public RegistryData(String name) {
-		this.resName = name;
+		this.resourceName = name;
 	}
 
 	public float getPositionX() {
-		return posX;
+		return positionX;
 	}
 
-	/**
-	 * 
-	 * @param posX
-	 */
 	public void setPositionX(float posX) {
-		this.posX = posX;
+		this.positionX = posX;
 	}
 
 	public float getPositionY() {
-		return posY;
+		return positionY;
 	}
 
-	public void setPositonY(float posY) {
-		this.posY = posY;
+	public void setPositionY(float posY) {
+		this.positionY = posY;
 	}
-
-
 
 	/**
 	 * Get the name that will be the resource name
@@ -52,7 +54,7 @@ public class RegistryData implements Serializable {
 	 */
 	public String getResourceName() {
 
-		return this.resName;
+		return this.resourceName;
 	}
 
 	/**
@@ -62,7 +64,39 @@ public class RegistryData implements Serializable {
 	 *            The user-defined resource name
 	 */
 	public void setResourceName(String name) {
-		this.resName = name;
+		this.resourceName = name;
 	}
 
+	public IResourceAgent getAgent() {
+		return agent;
+	}
+
+
+	public IResourceAgent getStub() {
+		return stub;
+	}
+
+	public void setStub(IResourceAgent stub) {
+		this.stub = stub;
+	}
+	
+	public void setAgent(IResourceAgent agent) {
+		this.agent = agent;
+	}
+
+	public boolean isFake() {
+		return fake;
+	}
+
+	public void setFake(boolean simulated) {
+		this.fake = simulated;
+	}
+
+	public int getIconType() {
+		return iconType;
+	}
+
+	public void setIconType(int iconType) {
+		this.iconType = iconType;
+	}
 }
