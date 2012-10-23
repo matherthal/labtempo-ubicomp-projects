cb76a32d425459899c4a58813c5978f3a836d049
matherthal <matherthal@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-10-23 21:11:55 +0000

    implementing the tree abstract data in RuleInterpreter

---
+++  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/Predicate.java
@@ -0,0 +1,223 @@
+package br.uff.tempo.middleware.management;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.uff.tempo.middleware.comm.current.api.Tuple;
+import br.uff.tempo.middleware.management.stubs.Stub;
+import br.uff.tempo.middleware.management.utils.datastructure.GeneralTree;
+
+public class Predicate extends Formula {
+	// First operand information
+	private Operand op1 = null;
+
+	// Second operand information
+	private Operand op2 = null;
+
+	// Operator
+	private Operator operator = Operator.Equal;
+
+	// constVal is the value to compare the the change of the context variable
+	// The constant value can be the first or second operand
+	// private Object constVal;
+
+//	private long timeout = 0;
+	private boolean valid = false;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param ra_op2
+	 * @param cv_op2
+	 * @param operator
+	 * @throws Exception
+	 */
+	public Predicate(Operand op1, Operator operator, Operand op2, long timeout) throws Exception {
+		super();
+		this.op1 = op1;
+		this.op2 = op2;
+		this.operator = operator;
+		this.setTimeout(timeout);
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param ra_op2
+	 * @param cv_op2
+	 * @param operator
+	 * @throws Exception
+	 */
+	public Predicate(Operand op, long timeout) throws Exception {
+		super();
+		this.op1 = op;
+		this.setTimeout(timeout);
+	}
+
+	public boolean evaluate() {
+		Object v1 = this.op1.getVal();
+		Object v2 = this.op2.getVal();
+
+		// Parse value to attribute's type. Ex., if it is boolean, so we parse
+		// the value to boolean before compare
+		if (operator.equals(Operator.Equal)) // Operator ==
+			this.valid = v1.equals(v2);
+		else if (operator.equals(Operator.Different)) // Operator !=
+			this.valid = !v1.equals(v2);
+		else {
+			double d_v2 = Double.parseDouble(v2.toString());
+			double d_v1 = Double.parseDouble(v1.toString());
+			if (operator.equals(Operator.GreaterThan))
+				this.valid = d_v1 > d_v2;
+			else if (operator.equals(Operator.GreaterThanOrEqual))
+				this.valid = d_v1 >= d_v2;
+			else if (operator.equals(Operator.LessThan))
+				this.valid = d_v1 < d_v2;
+			else if (operator.equals(Operator.LessThanOrEqual))
+				this.valid = d_v1 <= d_v2;
+			else
+				this.valid = false;
+		}
+		return isValid();
+	}
+
+	// @Override
+	// public String toString() {
+	// // return this.rai() + " " + this.method + " " + this.operator + " " +
+	// // this.value;
+	// return "";
+	// }
+
+	/**
+	 * @return the op1
+	 */
+	public Operand getOp1() {
+		return op1;
+	}
+
+	/**
+	 * @param op1
+	 *            the op1 to set
+	 */
+	public void setOp1(Operand op1) {
+		this.op1 = op1;
+	}
+
+	/**
+	 * @return the op2
+	 */
+	public Operand getOp2() {
+		return op2;
+	}
+
+	/**
+	 * @param op2
+	 *            the op2 to set
+	 */
+	public void setOp2(Operand op2) {
+		this.op2 = op2;
+	}
+
+	/**
+	 * @param valid
+	 *            the valid to set
+	 */
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+
+	/**
+	 * @return the valid
+	 */
+	public boolean isValid() {
+		return valid;
+	}
+
+	/**
+	 * @return the valueCache
+	 */
+	// public Object getValueCache() {
+	// return value_op1;
+	// }
+
+	/**
+	 * @param valueCache
+	 *            the valueCache to set
+	 */
+	// public void setValueCache(Object valueCache) {
+	// this.value_op1 = valueCache;
+	// // if (!test())
+	// // timerReset();
+	// }
+
+	/**
+	 * Update value stored in valueCache, result from context variable query or
+	 * notification
+	 */
+	// public void updateValueCache() {
+	// Stub s = new Stub(rai_op1);
+	// setValueCache(s.makeCall(cv_op1, params_op1));
+	// }
+
+	// /**
+	// * @return the value
+	// */
+	// public Object getConstValue() {
+	// return constVal;
+	// }
+	//
+	// /**
+	// * @param value
+	// * the value to set
+	// */
+	// public void setConstValue(Object value) {
+	// this.constVal = value;
+	// }
+
+	// /**
+	// * @return the rai
+	// */
+	// public String getRai() {
+	// return rai_op1;
+	// }
+	//
+	// /**
+	// * @return the method
+	// */
+	// public String getCV() {
+	// return cv_op1;
+	// }
+	//
+	// /**
+	// * @return the value
+	// */
+	// public void setCV(String cv, Object[] params) {
+	// if (op == 1) {
+	// this.cv_op1 = cv;
+	// }
+	//
+	// }
+
+	/**
+	 * Constructor
+	 * 
+	 * @param ra
+	 * @param cv
+	 * @param operator
+	 * @param constVal
+	 *            It creates a condition
+	 * @throws Exception
+	 */
+	// public ComparisonNode(Operand op, Operator operator, Object value, long
+	// timeout) throws Exception {
+	// super(0);
+	// this.rai_op1 = rai;
+	// this.setCV(cv, params);
+	//
+	// this.rai_op2 = null;
+	// this.setCV(null, null);
+	// this.op1 = op;
+	// this.operator = operator;
+	// this.constVal = value;
+	// }
+}
---  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/RuleInterpreter.java
+++  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/RuleInterpreter.java
@@ -3,26 +3,39 @@ package br.uff.tempo.middleware.management;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import net.sourceforge.jeval.EvaluationException;
+import net.sourceforge.jeval.Evaluator;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.google.gson.JsonObject;
 
 import android.util.Log;
 import br.uff.tempo.middleware.management.interfaces.IResourceAgent;
 import br.uff.tempo.middleware.management.interfaces.IResourceDiscovery;
 import br.uff.tempo.middleware.management.stubs.ResourceAgentStub;
 import br.uff.tempo.middleware.management.stubs.ResourceDiscoveryStub;
+import br.uff.tempo.middleware.management.utils.datastructure.GeneralTree;
 
 public class RuleInterpreter extends ResourceAgent {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private String TAG = "RuleInterpreter";
 
 	public static final String RULE_TRIGGERED = "ruleTrigger";
+	public static final int PRIORITY_LOW = 2;
+	public static final int PRIORITY_MED = 1;
+	public static final int PRIORITY_HIGH = 0;
 
-	private Set<ComparisonNode> cNSet = new HashSet<ComparisonNode>();
+	private Set<Predicate> cNSet = new HashSet<Predicate>();
 	private IResourceDiscovery discovery;
 	private boolean valid = false;
 	private Timer timer = null;
@@ -30,6 +43,10 @@ public class RuleInterpreter extends ResourceAgent {
 	private List<Integer> timeoutList = new ArrayList();
 	// FIXME: this will not belong to the rule, but to the comparison node
 	private Integer timeout = 0;
+	private Integer priority = PRIORITY_LOW;
+	private Formula formula;
+	private Formula pNode;
+	private int i_debug = 0;
 
 	public RuleInterpreter(String name) {
 		super(name, "br.uff.tempo.middleware.management.RuleInterpreter", 9);
@@ -42,12 +59,182 @@ public class RuleInterpreter extends ResourceAgent {
 	}
 
 	@Service(name = "Definir expressão")
-	public void setExpression(String expr) {
-		parseExpression(expr);
+	public void setExpression(String expr) throws Exception {
+		parseExpression(expr.replace('\t', ' ').replace('\n', ' ').trim());
 	}
 
-	private void parseExpression(String expr) {
-		// Condition cond = new Condition(rRS, expr, expr, cond);
+	private void parseExpression(String expr) throws Exception {
+		JSONObject intp = new JSONObject(expr).getJSONObject("interpreter");
+
+		Iterator it = intp.keys();
+		String key = "";
+		boolean hasForm = false;
+		while (it.hasNext()) {
+			key = it.next().toString();
+			if (key.equals("name")) {
+				super.setName(intp.getString(key));
+				Log.i("NAME", intp.getString(key));
+			} else if (key.equals("priority"))
+				this.priority = intp.getInt(key);
+			else if (key.equals("formula")) {
+				if (hasForm)
+					throw new Exception("Interpreter has two formulas");
+				formula = new Formula();
+				pNode = formula;
+				formula.attachFormula(buildTree(intp.getJSONArray(key)));
+				hasForm = true;
+			}
+		}
+	}
+
+	private Formula buildTree(JSONArray jsonSubtree) throws Exception {
+		Formula subtree = new Formula();
+		Object o;
+		for (int i = 0; i < jsonSubtree.length(); i++) {
+			o = jsonSubtree.get(i);
+			if (o.getClass().equals(JSONObject.class)) {
+				// Context, timer clause, not clause and connectives are objects
+				// in JSON
+				JSONObject jobj = (JSONObject) o;
+				Iterator it = jobj.keys();
+				String key = "";
+				Object val = "";
+				while (it.hasNext()) {
+					key = it.next().toString();
+					val = jobj.get(key);
+					if (key.equals("predicate")) {
+						Log.i(TAG, "PREDICATE: " + val.toString());
+						subtree.attachFormula(getPredicate((JSONArray) val));
+					} else if (key.equals("timer")) {
+						Log.i(TAG, "TIMER: " + val.toString());
+						subtree.setTimeout(getTimerInSec(val.toString()));
+					} else if (key.equals("connective")) {
+						Log.i(TAG, "CONNECTIVE: " + val.toString());
+					} else if (key.equals("not")) {
+						Log.i(TAG, "NOT CLAUSE: " + val.toString());
+					} else if (key.equals("formula")) {
+						Log.i(TAG, "FORMULA: " + val.toString());
+						subtree.attachFormula(buildTree((JSONArray) val));
+					} else {
+						Log.e(TAG, "UNKNOWN: " + jobj.get(key).toString());
+						throw new Exception("Unknown object in JSON file: " + o.toString());
+					}
+				}
+			} else {
+				throw new Exception("Unknown object in JSON file: " + o.toString());
+			}
+		}
+		return subtree;
+	}
+
+	private Predicate getPredicate(JSONArray jsonPredicate) throws Exception {
+		Operand op1 = null;
+		Operand op2 = null;
+		Operator op = null;
+		Integer timer = 0;
+
+		// Get each component from jsonPredicate (context_elem, op, value,
+		// timer) to create the subtree
+		JSONObject pred;
+		for (int i = 0; i < jsonPredicate.length(); i++) {
+			if (jsonPredicate.isNull(i))
+				throw new Exception("Null Predicate: " + jsonPredicate);
+			pred = (JSONObject) jsonPredicate.get(i);
+			// If the component is a context_elem, get its attributes (rai,
+			// elem, params) and put it first into op1 and next in op2
+			String s_component = pred.keys().next().toString();
+			// JSONObject component = (JSONObject) pred.get(s_component);
+			Object component = pred.get(s_component);
+
+			if ("context_elem".equals(s_component)) {
+				JSONObject jobj = (JSONObject) component;
+				// Get rai
+				String rai = jobj.getString("rai");
+				// Get elem
+				String elem = jobj.getString("elem");
+				// Get params (optional)
+				Object[] params = new Object[0];
+				if (jobj.has("params")) {
+					String s_params = jobj.getString("params");
+					params = s_params.split(",");
+				}
+
+				// If op1 has already been got, put operand in op2
+				if (op1 == null)
+					op1 = new Operand(rai, elem, params);
+				else if (op2 == null)
+					op2 = new Operand(rai, elem, params);
+				else
+					throw new Exception(
+							"Wrong parameters in Interpreter's Predicate: "
+									+ jsonPredicate);
+				// Get Value
+			} else if ("val".equals(s_component)) {
+				// If op1 has already been got, put operand in op2
+				if (op1 == null)
+					op1 = new Operand(component.toString());
+				else if (op2 == null)
+					op2 = new Operand(component.toString());
+				else
+					throw new Exception(
+							"Wrong parameters in Interpreter's Predicate: "
+									+ jsonPredicate);
+				// Get operator
+			} else if ("op".equals(s_component)) {
+				op = getOperator(component.toString());
+				// Get timer
+			} else if ("timer".equals(s_component)) {
+				timer = getTimerInSec(component.toString());
+			} else
+				throw new Exception(
+						"Unknown timer in Interpreter's Predicate: "
+								+ jsonPredicate);
+		}
+
+		// If the second operand has not been given, then probably the first one
+		// is boolean and can be evaluated alone
+		if (op2 == null)
+			return new Predicate(op1, 0);
+		// If there are 2 operands, then it's mandatory having an operator
+		// between them
+		else if (op != null)
+			return new Predicate(op1, op, op2, 0);
+		else
+			throw new Exception(
+					"Wrong parameters in Interpreterr's Predicate: "
+							+ jsonPredicate);
+	}
+
+	private Operator getOperator(String op) throws Exception {
+		if (op.equals("="))
+			return Operator.Equal;
+		else if (op.equals("!="))
+			return Operator.Different;
+		else if (op.equals(">"))
+			return Operator.GreaterThan;
+		else if (op.equals("<"))
+			return Operator.LessThan;
+		else if (op.equals(">="))
+			return Operator.GreaterThanOrEqual;
+		else if (op.equals("<="))
+			return Operator.LessThanOrEqual;
+		else
+			throw new Exception("Unknown operator: " + op);
+	}
+
+	private int getTimerInSec(String timer) throws Exception {
+		String time = timer.toString().split("[A-Za-z]+")[0];
+		String order = timer.toString().split("[0-9]+")[1];
+		int i_timer = Integer.parseInt(time); // Timer default in seconds
+		if ("msec".equals(order))
+			i_timer = i_timer / 1000;
+		else if ("min".equals(order))
+			i_timer = i_timer * 60;
+		else if ("h".equals(order))
+			i_timer = i_timer * 3600;
+		else if (!"sec".equals(order))
+			throw new Exception("Unknown timer: " + timer);
+		return i_timer;
 	}
 
 	/**
@@ -60,11 +247,14 @@ public class RuleInterpreter extends ResourceAgent {
 	}
 
 	@Deprecated
-	public void setCondition(String rai, String cv, Object[] params, Operator op, Object value) throws Exception {
+	public void setCondition(String rai, String cv, Object[] params,
+			Operator op, Object value) throws Exception {
 		IResourceAgent ra = new ResourceAgentStub(rai);
 		ra.registerStakeholder(cv, this.getRAI());
 		// re discovery.search(rai).get(0);
-		cNSet.add(new ComparisonNode(rai, cv, params, op, value, 0));
+		// cNSet.add(new ComparisonNode(rai, cv, params, op, value, 0));
+		cNSet.add(new Predicate(new Operand(rai, cv, params), op, new Operand(
+				value), 0));
 	}
 
 	// @Deprecated
@@ -83,9 +273,18 @@ public class RuleInterpreter extends ResourceAgent {
 	 * this.
 	 */
 	private boolean evaluateExpr() {
+		//TODO: implement evaluator using jeval
+//		try {
+//			Evaluator ev = new Evaluator();
+//			Log.i(TAG, "Eval: " + ev.getBooleanResult("1 && (1 || 0)"));
+//		} catch (EvaluationException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
 		// boolean prevValid = valid;
 		valid = true; // Temporary. If it's false, it'll turn again to false
-		for (ComparisonNode cn : cNSet) {
+		for (Predicate cn : cNSet) {
 			if (!cn.evaluate()) {
 				valid = false;
 				break;
@@ -108,14 +307,15 @@ public class RuleInterpreter extends ResourceAgent {
 	public void notificationHandler(String rai, String method, Object value) {
 		Log.i(TAG, "Notification received");
 
-		for (ComparisonNode cn : cNSet) {
+		for (Predicate cn : cNSet) {
 			// If the change comes from the correct agent AND the context
 			// variable (method) is the same AND the value is different,
 			// then evaluate
-			if (cn.getRai().equals(rai) && cn.getMethod().equals(method)) {
+			Operand op = cn.getOp1();
+			if (op.getRai().equals(rai) && op.getCv().equals(method)) {
 				boolean prevValid = cn.isValid();
 
-				cn.setValueCache(value);
+				op.setValue(value);
 				// The expression only will be evaluated if the current
 				// validation of the rule and the context variable are false.
 				// Otherwise it will be assumed that the expression didn't came
