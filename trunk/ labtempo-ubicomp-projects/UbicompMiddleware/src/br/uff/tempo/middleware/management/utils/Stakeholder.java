a8c366300abfc8e63e36c0a388a3c317264eeca1
andre.ribeiro.coelho@gmail.com <andre.ribeiro.coelho@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-10-25 19:30:29 +0000

    ResourceData removed from Stakeholder.

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/utils/Stakeholder.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/utils/Stakeholder.java
@@ -1,34 +1,19 @@
 package br.uff.tempo.middleware.management.utils;
 
 import java.io.Serializable;
-import java.util.List;
 
-import android.util.Log;
 import br.uff.tempo.middleware.management.ResourceAgent;
-import br.uff.tempo.middleware.management.ResourceData;
-import br.uff.tempo.middleware.management.interfaces.IResourceDiscovery;
-import br.uff.tempo.middleware.management.stubs.ResourceDiscoveryStub;
 
 public class Stakeholder implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	private String method;
 	private String rai;
-	private String name;
-	private ResourceData data;
 
 	public Stakeholder(String method, String rai) {
 		this.method = method;
 		this.rai = rai;
-		
-		this.name = new ResourceAgentIdentifier(rai).getName();
-		
-		List<ResourceData> resData = new ResourceDiscoveryStub(IResourceDiscovery.RDS_ADDRESS).searchForAttribute(ResourceData.NAME, this.name);
-		
-		if (resData != null) {
-			data = resData.get(0);
-		} else {
-			Log.i("Stakeholder class", "The name " + this.name + " was NOT found in the repository by searchForAttribute method!");
-		}
 	}
 
 	public Stakeholder(String method, ResourceAgent rA) {
@@ -41,14 +26,6 @@ public class Stakeholder implements Serializable {
 
 	public String getRAI() {
 		return rai;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public ResourceData getResourceData() {
-		return data;
 	}
 	
 	@Override
@@ -67,7 +44,6 @@ public class Stakeholder implements Serializable {
 	
 	@Override
 	public String toString() {
-		
-		return this.name + " wants: " + this.method;
+		return this.rai + " wants: " + this.method;
 	}
 }
