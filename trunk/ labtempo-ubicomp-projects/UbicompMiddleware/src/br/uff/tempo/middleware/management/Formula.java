e700092125a7b236dcda7ad3bff78b0e77b9e71d
matherthal <matherthal@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2012-10-27 13:19:28 +0000

    implementing "not" clause

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/Formula.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/Formula.java
@@ -4,6 +4,21 @@ import br.uff.tempo.middleware.management.utils.datastructure.GeneralTree;
 
 public class Formula extends GeneralTree {
 	private long timeout = 0;
+	private boolean notClause = false; 
+
+	/**
+	 * @return the notClause
+	 */
+	public boolean isNotClause() {
+		return notClause;
+	}
+
+	/**
+	 * @param notClause the notClause to set
+	 */
+	public void setNotClause(boolean notClause) {
+		this.notClause = notClause;
+	}
 
 	public Formula() {
 		super(0);
