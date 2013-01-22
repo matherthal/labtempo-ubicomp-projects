115dd0ea19256ecb84556afaff6b42e2ee320174
matherthal@gmail.com <matherthal@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2013-01-22 18:56:57 +0000

    changed superclass

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/OrNode.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/middleware/management/OrNode.java
@@ -2,10 +2,8 @@ package br.uff.tempo.middleware.management;
 
 import br.uff.tempo.middleware.management.utils.datastructure.GeneralTree;
 
-public class OrNode extends GeneralTree {
-
+public class OrNode extends Formula {
 	public OrNode() {
 		super("||");
 	}
-
 }
