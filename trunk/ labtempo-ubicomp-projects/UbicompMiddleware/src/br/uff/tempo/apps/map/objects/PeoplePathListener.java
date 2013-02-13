97350e383d3b88b316df2b7da284b2d746b04f02
david.brto <david.brto@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2013-02-13 14:40:16 +0000

    Classes to help in separate of concerns

---
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/objects/PeoplePathListener.java
@@ -0,0 +1,49 @@
+package br.uff.tempo.apps.map.objects;
+
+import org.andengine.entity.IEntity;
+import org.andengine.entity.modifier.PathModifier;
+import org.andengine.entity.modifier.PathModifier.IPathModifierListener;
+import org.andengine.entity.sprite.AnimatedSprite;
+
+public class PeoplePathListener implements IPathModifierListener {
+
+	@Override
+	public void onPathWaypointStarted(final PathModifier pPathModifier,
+			final IEntity pEntity, final int pWaypointIndex) {
+
+		final int nextWaypoint = (pWaypointIndex >= pPathModifier.getPath()
+				.getLength()) ? 0 : pWaypointIndex + 1;
+		final float pathX = pPathModifier.getPath().getCoordinatesX()[nextWaypoint];
+		final float pathY = pPathModifier.getPath().getCoordinatesY()[nextWaypoint];
+		final AnimatedSprite sprite = (AnimatedSprite) pEntity;
+
+		if (pathX > sprite.getX()) {
+			// Right
+			sprite.animate(new long[] { 200, 200, 200 }, 6, 8, true);
+		} else if (pathX < sprite.getX()) {
+			// Left
+			sprite.animate(new long[] { 200, 200, 200 }, 3, 5, true);
+		} else if (pathY > sprite.getY()) {
+			// Down
+			sprite.animate(new long[] { 200, 200, 200 }, 0, 2, true);
+		} else if (pathY < sprite.getY()) {
+			// Up
+			sprite.animate(new long[] { 200, 200, 200 }, 9, 11, true);
+		}
+	}
+
+	@Override
+	public void onPathWaypointFinished(final PathModifier pPathModifier,
+			final IEntity pEntity, final int pWaypointIndex) {	}
+
+	@Override
+	public void onPathStarted(final PathModifier pPathModifier,
+			final IEntity pEntity) {}
+
+	@Override
+	public void onPathFinished(final PathModifier pPathModifier,
+			final IEntity pEntity) {
+		((AnimatedSprite) pEntity).stopAnimation(1);
+	}
+
+}
