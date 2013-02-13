4212a6cfe950664681d1b636df1fdd1b7151cefd
david.brto <david.brto@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2013-02-13 14:31:50 +0000

    Adapted to use an interface that listen to the events

---  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/dialogs/ChooseResource.java
+++  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/dialogs/ChooseResource.java
@@ -2,7 +2,6 @@ package br.uff.tempo.apps.map.dialogs;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.view.View;
@@ -19,11 +18,12 @@ public class ChooseResource extends MapDialog implements
 	private ArrayAdapter<String> lvAdapter;
 	private List<ResourceData> dataFromResouces;
 	private String[] listNames;
+	private IChooser chooser;
 
-	public ChooseResource(final Activity act) {
-
+	public ChooseResource(final Activity act, final IChooser chooser) {
 		super(act, R.layout.registered_resource_list, R.string.title_config_res);
 
+		this.chooser = chooser;
 		act.runOnUiThread(new Runnable() {
 
 			@Override
@@ -32,36 +32,34 @@ public class ChooseResource extends MapDialog implements
 				// Get the reference to the list view
 				listView = (ListView) dialog.findViewById(R.id.list_registered);
 				listView.setOnItemClickListener(ChooseResource.this);
-
 				// Cancel the dialog if you touch outside its area.
 				dialog.setCanceledOnTouchOutside(true);
 			}
 		});
 	}
+	
+	public ChooseResource(final Activity act) {
+		this(act, (IChooser) act);
+	}
 
 	public void showDialog(String[] list) {
-
 		listNames = list;
 		lvAdapter = new ArrayAdapter<String>(activity,
 				android.R.layout.simple_list_item_1, list);
 
 		listView.setAdapter(lvAdapter);
-
 		super.showDialog();
 	}
 
 	public void showDialog(List<ResourceData> list) {
-
 		dataFromResouces = formatList(list);
 		String[] names = new String[dataFromResouces.size()];
 		
 		for (int i = 0; i < dataFromResouces.size(); i++) {
 			ResourceData data = dataFromResouces.get(i);
-			
 			// Show in the list a custom tag, if it exits. Show the name otherwise
 			names[i] = data.getTag() != null ? data.getTag() : data.getName();
 		}
-		
 		showDialog(names);
 	}
 
@@ -70,26 +68,21 @@ public class ChooseResource extends MapDialog implements
 		List<ResourceData> list = new ArrayList<ResourceData>();
 
 		for (ResourceData data : resList) {
-
 			if (!data.getName().contains("Resource")) {
 				list.add(data);
 			}
 		}
-
 		return list;
 	}
 
 	@Override
-	public void onClick(View v) {
-
-	}
+	public void onClick(View v) {}
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
 
-		IChooser chooser = (IChooser) activity;
-		ChoosedData data = new ChoosedData();
+		ChosenData data = new ChosenData();
 
 		if (dataFromResouces != null) {
 			data.setData(dataFromResouces.get(position));
@@ -97,8 +90,7 @@ public class ChooseResource extends MapDialog implements
 			data.setTag(listNames[position]);
 		}
 		
-		chooser.onRegisteredResourceChoosed(data);
-
+		chooser.onResourceChosen(data);
 		dialog.cancel();
 	}
 	
