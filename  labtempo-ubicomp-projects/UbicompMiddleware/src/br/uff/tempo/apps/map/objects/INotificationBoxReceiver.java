8e266be61f576614475456c04a675b75a34ccbd9
david.brto <david.brto@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2013-02-13 14:42:16 +0000

    Removed unnecessary methods

---  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/objects/INotificationBoxReceiver.java
+++  labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/objects/INotificationBoxReceiver.java
@@ -1,15 +1,6 @@
 package br.uff.tempo.apps.map.objects;
 
-import java.io.Serializable;
-
-import org.andengine.util.color.Color;
-
 public interface INotificationBoxReceiver {
 
 	public void showMessage(String message);
-
-	public void setNotificationBoxColor(Color color);
-
-	public void setNotificationBoxVisible(boolean visible);
-	
 }
