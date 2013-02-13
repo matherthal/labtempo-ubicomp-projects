2b6472e45dcb3d0e3f0aaa62705fac445a6bb233
david.brto <david.brto@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2013-02-13 14:27:46 +0000

    Minor changes

--- trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/simulators/creator/SimulCreatorActivity.java
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/simulators/creator/SimulCreatorActivity.java
@@ -1,23 +1,15 @@
 package br.uff.tempo.apps.simulators.creator;
 
-import java.util.List;
-
 import android.app.Activity;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import br.uff.tempo.R;
-import br.uff.tempo.apps.map.dialogs.ChoosedData;
-import br.uff.tempo.apps.map.dialogs.IChooser;
-import br.uff.tempo.apps.map.dialogs.IDialogFinishHandler;
-import br.uff.tempo.apps.map.dialogs.IListGetter;
 import br.uff.tempo.apps.simulators.utils.Creator;
-import br.uff.tempo.middleware.management.ResourceData;
 
-public class SimulCreatorActivity extends Activity implements IChooser, IListGetter, IDialogFinishHandler {
+public class SimulCreatorActivity extends Activity {
 
 	private Button btnChoose;
 	private Button btnCreate;
@@ -45,23 +37,8 @@ public class SimulCreatorActivity extends Activity implements IChooser, IListGet
 	}
 
 	@Override
-	public void onRegisteredResourceChoosed(ChoosedData choosedData) {
-		creator.onRegisteredResourceChoosed(choosedData);
-	}
-
-	@Override
-	public void onGetList(List<ResourceData> result) {
-		creator.onGetList(result);
-	}
-
-	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.simul_creator, menu);
 		return true;
-	}
-
-	@Override
-	public void onDialogFinished(Dialog dialog) {
-		creator.onDialogFinished(dialog);		
 	}
 }
