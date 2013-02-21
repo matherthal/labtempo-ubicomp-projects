2b1669aa44e265d4a93d7d1921a0d18cece88868
david.brto@gmail.com <david.brto@gmail.com@8fdd17df-c619-e9a7-02e2-39ed4377ac6f>
2013-02-21 19:54:53 +0000

    New packages, to better organize the classes

---
+++ trunk/ labtempo-ubicomp-projects/UbicompMiddleware/src/br/uff/tempo/apps/map/objects/sprite/SmartSprite.java
@@ -0,0 +1,82 @@
+package br.uff.tempo.apps.map.objects;
+
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+import android.util.Log;
+import br.uff.tempo.apps.map.TouchEvents;
+import br.uff.tempo.apps.simulators.utils.ResourceWrapper;
+
+public class SmartSprite extends Sprite implements TouchEvents.ITouchEvents {
+
+	private TouchEvents touch;
+	private ISpriteControler spriteControler;
+	private ResourceWrapper wrapper;
+
+	public SmartSprite(float pX, float pY, ITextureRegion pTextureRegion,
+			VertexBufferObjectManager pVertexBufferObjectManager) {
+
+		super(pX, pY, pTextureRegion, pVertexBufferObjectManager);
+		
+		this.touch = new TouchEvents(this);
+	}
+	
+	public void registerSpriteControler(ISpriteControler spriteControler) {
+		this.spriteControler = spriteControler;
+	}
+
+	@Override
+	public boolean onAreaTouched(final TouchEvent pSceneTouchEvent,
+			float pTouchAreaLocalX, float pTouchAreaLocalY) {
+
+		touch.onAreaTouched(pSceneTouchEvent, pTouchAreaLocalX, pTouchAreaLocalY);
+		
+		return true;
+	}
+
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+
+	@Override
+	public void onStartLongPress(TouchEvent pSceneTouchEvent) {
+		Log.i("SmartAndroid", "onStartLongPress");
+		if (spriteControler != null) {
+			spriteControler.onSpriteStartLongPress(this, pSceneTouchEvent);
+		}
+	}
+
+	@Override
+	public void onLongPressMove(TouchEvent pSceneTouchEvent) {
+		Log.i("SmartAndroid", "onLongPressMove");
+		if (spriteControler != null) {
+			spriteControler.onSpriteLongPressMove(this, pSceneTouchEvent);
+		}
+	}
+
+	@Override
+	public void onEndLongPressMove(TouchEvent pSceneTouchEvent) {
+		Log.i("SmartAndroid", "onEndLongPressMove");
+		if (spriteControler != null) {
+			spriteControler.onSpriteEndLongPressMove(this, pSceneTouchEvent);
+		}
+	}
+
+	@Override
+	public void onTap(TouchEvent pSceneTouchEvent) {
+		Log.i("SmartAndroid", "onTap");
+		if (spriteControler != null) {
+			spriteControler.onSpriteTap(this, pSceneTouchEvent);
+		}
+	}
+
+	public ResourceWrapper getResourceWrapper() {
+		return wrapper;
+	}
+
+	public void setResourceWrapper(ResourceWrapper wrapper) {
+		this.wrapper = wrapper;
+	}
+}
